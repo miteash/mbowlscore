ad5380abf9cbc8f4e6d00a0753a272783bb11aa3
miteash <miteashthaker10@gmail.com>
2022-05-04 20:35:38 -0400

    Logic Correction

--- src/main/java/com/mt/Throw.java
+++ src/main/java/com/mt/Throw.java
@@ -1,2 +1,19 @@
-package com.mt;public class Throw {
+package com.mt;
+
+public class Throw {
+    private int knockedDownPins;
+    public boolean done = false;
+    public int throwNumber =0;
+
+    public int getKnockedDownPins() {
+        return knockedDownPins;
+    }
+
+    public void setKnockedDownPins(int knockedDownPins) {
+        this.knockedDownPins = knockedDownPins;
+    }
+
+    public Integer getKnockedDownPinsAsInteger(){
+        return knockedDownPins;
+    }
 }
